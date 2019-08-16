ba85e6454795fc778bd9306d3bc08aa8113dff43
Thorsten Kukuk <kukuk@suse.com>
2019-08-16 10:36:44 +0200

    Fix script and make sure all required sources are included

--- update_source.sh
+++ update_source.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -x
 
+GLIBC_CHECKOUT=../../glibc
+
 for i in files-grp.c files-initgroups.c files-parse.c files-pwd.c files-spwd.c files-XXX.c files-alias.c files-ethers.c files-proto.c files-rpc.c files-service.c ; do
-	cp -a ../glibc/nss/nss_files/$i src/
+	cp -a ${GLIBC_CHECKOUT}/nss/nss_files/$i src/
 done
 sed -i -e 's|_nss_files_|_nss_usrfiles_|g' src/files-XXX.c
 sed -i -e 's|_nss_files_|_nss_usrfiles_|g' src/files-alias.c
@@ -11,7 +13,7 @@ sed -i -e 's|/etc/|/usr/etc/|g' src/files-XXX.c
 sed -i -e 's|/etc/|/usr/etc/|g' src/files-initgroups.c
 sed -i -e 's|/etc/|/usr/etc/|g' src/files-alias.c
 sed -i -e 's|#include <netinet/ether.h>|#include "ether.h"|g' src/files-ethers.c
-cp -a ../glibc/include/netinet/ether.h src
+cp -a ${GLIBC_CHECKOUT}/include/netinet/ether.h src
 sed -i -e 's|<inet/netinet/ether.h>|<netinet/ether.h>|g' src/ether.h
 sed -i -e 's|^libc_hidden_proto.*||g' src/ether.h
 sed -i -e 's|^libnss_files_hidden_proto.*||g' src/ether.h
